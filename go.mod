c1e2996cdb0b8be7dc518739b5b6a419c6b1ed38
Tyler Moody <tcmoody716@gmail.com>
2025-06-29 10:17:43 -0600

    Initial commit with baseline functionality. Able to retrieve all km's for corp and generate report.

---
+++ go.mod
@@ -0,0 +1,15 @@
+module killmails
+
+go 1.23.4
+
+require (
+	github.com/antihax/goesi v0.0.0-20241216030825-f102f993c3b1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+	golang.org/x/oauth2 v0.0.0-20211028175245-ba495a64dcb5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ killmails.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/antihax/goesi"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	email := os.Args[1]
+	targetMonth, err := strconv.Atoi(os.Args[2])
+	targetYear, err := strconv.Atoi(os.Args[3])
+
+	if err != nil {
+		panic(err)
+	}
+
+	var zkills []ZkillMailEntry
+	page := 1
+	for {
+		var zkillsPage []ZkillMailEntry
+		resp, err := http.Get(fmt.Sprintf("https://zkillboard.com/api/corporationID/98732555/year/%d/month/%d/page/%d/", targetYear, targetMonth, page))
+		if err != nil {
+			panic(err)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		json.Unmarshal(body, &zkillsPage)
+		if len(zkillsPage) == 0 {
+			break
+		}
+		zkills = append(zkills, zkillsPage...)
+		page++
+	}
+	var killmailsOutputs [][]KillMailOutput
+
+	// create ESI client
+	var ctx context.Context
+	client := http.Client{}
+	esiClient := goesi.NewAPIClient(&client, email)
+
+	fmt.Println("Beginning processing of killmails")
+	for _, value := range zkills {
+		fmt.Println(value)
+		//killmail, _, err := esiClient.ESI.KillmailsApi.GetKillmailsKillmailIdKillmailHash(ctx, value.KillmailHash, value.KillmailId, nil)
+		killmail, _, err := esiClient.ESI.KillmailsApi.GetKillmailsKillmailIdKillmailHash(ctx, value.ZKB.KillmailHash, value.KillmailId, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		if int(killmail.KillmailTime.Month()) != targetMonth {
+			continue
+		}
+
+		victim, _, err := esiClient.ESI.CharacterApi.GetCharactersCharacterId(ctx, killmail.Victim.CharacterId, nil)
+		if victim.CorporationId == 98732555 {
+			continue
+		}
+
+		victimShip, _, err := esiClient.ESI.UniverseApi.GetUniverseTypesTypeId(ctx, killmail.Victim.ShipTypeId, nil)
+
+		solarSystem, _, err := esiClient.ESI.UniverseApi.GetUniverseSystemsSystemId(ctx, killmail.SolarSystemId, nil)
+
+		killmailOutputs := make([]KillMailOutput, len(killmail.Attackers))
+		for index, charValue := range killmail.Attackers {
+			if charValue.CharacterId == 0 {
+				killmailOutputs[index] = KillMailOutput{
+					VictimId:         killmail.Victim.CharacterId,
+					VictimName:       victim.Name,
+					VictimShipId:     killmail.Victim.ShipTypeId,
+					VictimShipName:   victimShip.Name,
+					AttackerId:       charValue.CharacterId,
+					AttackerName:     "npc",
+					AttackerShipId:   charValue.ShipTypeId,
+					AttackerShipName: "npc",
+					TotalDamage:      charValue.DamageDone,
+					FinalBlow:        charValue.FinalBlow,
+					NumAttackers:     len(killmail.Attackers),
+					SolarSystemId:    killmail.SolarSystemId,
+					SolarSystemName:  solarSystem.Name,
+					SecurityLevel:    solarSystem.SecurityStatus,
+					KillMailTime:     killmail.KillmailTime,
+					TotalValue:       value.ZKB.TotalValue,
+				}
+				continue
+			}
+			character, _, err := esiClient.ESI.CharacterApi.GetCharactersCharacterId(ctx, charValue.CharacterId, nil)
+			if err != nil && err.Error() != "404 Not Found" {
+				panic(err)
+			} else if err != nil && err.Error() == "404 Not Found" {
+				continue
+			}
+			if character.CorporationId != 98732555 {
+				continue
+			}
+			attackerShip, _, err := esiClient.ESI.UniverseApi.GetUniverseTypesTypeId(ctx, charValue.ShipTypeId, nil)
+			killmailOutputs[index] = KillMailOutput{
+				VictimId:         killmail.Victim.CharacterId,
+				VictimName:       victim.Name,
+				VictimShipId:     killmail.Victim.ShipTypeId,
+				VictimShipName:   victimShip.Name,
+				AttackerId:       charValue.CharacterId,
+				AttackerName:     character.Name,
+				AttackerShipId:   charValue.ShipTypeId,
+				AttackerShipName: attackerShip.Name,
+				TotalDamage:      charValue.DamageDone,
+				FinalBlow:        charValue.FinalBlow,
+				NumAttackers:     len(killmail.Attackers),
+				SolarSystemId:    killmail.SolarSystemId,
+				SolarSystemName:  solarSystem.Name,
+				SecurityLevel:    solarSystem.SecurityStatus,
+				KillMailTime:     killmail.KillmailTime,
+				TotalValue:       value.ZKB.TotalValue,
+			}
+		}
+		killmailsOutputs = append(killmailsOutputs, killmailOutputs)
+	}
+
+	fmt.Println("Writing data to file")
+	WriteToFile(killmailsOutputs)
+}
+
+func WriteToFile(killmailsOutputs [][]KillMailOutput) {
+	f, err := os.Create("killmails.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString("victimId,victimName,victimShipId,victimShipName,attackerId,attackerName,attackerShipId,attackerShipName,totalDamage,finalBlow,numAttackers,solarSystemId,solarSystemName,securityLevel,killmailTime,totalValue\n"); err != nil {
+		panic(err)
+	}
+	f.Sync()
+
+	for _, output := range killmailsOutputs {
+		for _, killmail := range output {
+			if killmail.AttackerId == 0 && killmail.AttackerName != "npc" {
+				continue
+			}
+			line := fmt.Sprintf("%d,%s,%d,%s,%d,%s,%d,%s,%d,%t,%d,%d,%s,%f,%v,%f", killmail.VictimId, killmail.VictimName, killmail.VictimShipId, killmail.VictimShipName, killmail.AttackerId, killmail.AttackerName, killmail.AttackerShipId, killmail.AttackerShipName, killmail.TotalDamage, killmail.FinalBlow, killmail.NumAttackers, killmail.SolarSystemId, killmail.SolarSystemName, killmail.SecurityLevel, killmail.KillMailTime, killmail.TotalValue)
+			if _, err := f.WriteString(line + "\n"); err != nil {
+				panic(err)
+			}
+			f.Sync()
+		}
+	}
+}
+
+type KillMailEntry struct {
+	KillmailHash string `json:"killmail_hash"`
+	KillmailId   int32  `json:"killmail_id"`
+}
+
+// if _, err := f.WriteString("victimId,victimName,victimShipId,victimShipName,attackerId,attackerName,attackerShipId,attackerShipName,totalDamage,finalBlow,numAttackers,solarSystemId,solarSystemName,killmailTime\n"); err != nil {
+type KillMailOutput struct {
+	VictimId         int32
+	VictimName       string
+	VictimShipId     int32
+	VictimShipName   string
+	AttackerId       int32
+	AttackerName     string
+	AttackerShipId   int32
+	AttackerShipName string
+	TotalDamage      int32
+	FinalBlow        bool
+	NumAttackers     int
+	SolarSystemId    int32
+	SolarSystemName  string
+	SecurityLevel    float32
+	KillMailTime     time.Time
+	TotalValue       float32
+}
+
+type ZKB struct {
+	KillmailHash string  `json:"hash"`
+	TotalValue   float32 `json:"totalValue"`
+}
+
+type ZkillMailEntry struct {
+	KillmailId int32 `json:"killmail_id"`
+	ZKB        ZKB   `json:"zkb"`
+}
